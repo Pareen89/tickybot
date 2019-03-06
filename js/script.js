5671f78a72f8c5753728c42f5420cf83c568985f
pareen89 <pareenpatel89@gmail.com>
2019-03-05 17:21:57 -0800

    Updated scroll button

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ let about = document.getElementById("about");
 let feature = document.getElementById("feature");
 let header = document.getElementById("header");
 let scrollUp = document.getElementById("scroll");
-
+let card = document.getElementById("card");
 window.addEventListener("scroll", () => {
   let scrolled = window.scrollY;
   let aboutPosition = about.offsetTop;
-  if (Math.ceil(scrolled) === aboutPosition) {
+  console.log(aboutPosition);
+  if (Math.ceil(scrolled) >= aboutPosition) {
     scrollUp.style.display = "grid";
+  }
+  if (Math.ceil(scrolled) < aboutPosition) {
+    scrollUp.style.display = "none";
   }
 });
